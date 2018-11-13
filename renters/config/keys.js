c8cfab6619299cc5ed796874ccc56e397662044a
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-11-13 15:06:24 -0800

    added jsonwebtoken

--- renters/config/keys.js
+++ renters/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: 
-    'mongodb://bob:sanjo408@ds039778.mlab.com:39778/renters'
+    'mongodb://bob:sanjo408@ds039778.mlab.com:39778/renters',
+    secretOrKey: 'secret'
 }
