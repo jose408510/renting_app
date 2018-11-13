e76ea54798dad376e9af8f337a9bf1951919be16
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-11-13 14:11:39 -0800

    added bcrypt

--- renters/routes/api/users.js
+++ renters/routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = requrie('gravatar')
+const bcrypt = require('bcryptjs')
 // loading users
 const User = require('../../models/users')
 
@@ -19,13 +20,20 @@ router.get('/register' , (req,res) => {
                 r: 'pg', //rating
                 d: 'mm' // Default
             });
-
-
             const newUser = new User({
                 name: req.body.name,
                 email: req.body.email,
                 avatar,
                 password: req.body.password
+            });
+            bcrypt.genSalt(10 , ( err , salt ) => {
+                bcrypt.hash(newUser.password , salt , ( err, hash ) => {
+                    if(err) throw err ;
+                    newUser.password = hash;
+                    newUser.save()
+                    .then(user => res.json(user))
+                    .catch( err => console.log(err));
+                })
             })
         }
     }) 
