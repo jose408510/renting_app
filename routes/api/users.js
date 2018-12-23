3e4fefae45dade9bd8d1fba9252242047f788581
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-12-22 21:19:41 -0800

    fixed minor issue , routes now working and will start incorporating login auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,3 +1,30 @@
 const express = require('express');
-const User = require('../../models/Users');
+const router = express.Router();
 
+//Load User models
+const Users = require('../../models/Users');
+
+router.get('/test', ( req, res ) => res.json({msg:"Users Works"
+}))
+
+// @public route
+// @register new users 
+router.post('/signup', ( req, res ) => {
+    const newUser = new Users({
+        email: req.body.email,
+        password: req.body.email
+    })
+    console.log(newUser)
+    newUser.save()
+})
+
+// @public router
+// @sign-in for exisiting users 
+router.get('/newusers', ( req, res ) => {
+    res.json({
+        email: "pablo",
+        password: "jose@email"
+    })
+})
+
+module.exports = router;
