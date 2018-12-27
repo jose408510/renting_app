f8b816aec2a8fdececc617d384b6d577882305de
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-12-26 18:34:37 -0800

    signup route done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,29 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs'); 
 
+// Validation Flies
+const validateSignup = require('../../validation/signup')
 //Load User models
 const Users = require('../../models/Users');
 
 router.get('/test', ( req, res ) => res.json({msg:"Users Works"
 }))
 
-// @public route
 // @register new users 
 router.post('/signup', ( req, res ) => {
-    Users.findOne({email: req.body.email})
-    .then(user => {
+   
+    const { errors, isValid } = validateSignup(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        console.log(req.body)
+        return res.status(400).json(errors)
+    }
+    Users.findOne({email: req.body.email}).then(user => {
         if(user){
             return res.status(400).json('User Already Exist');
         } else{
             const newUser = new Users({
                 email: req.body.email,
-                password: req.body.email
+                password: req.body.password
+            });
+        bcrypt.genSalt(12, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err,hash) => {
+                if(err) throw err;
+                newUser.password = hash;
+                newUser.save()
+                .then(user => res.json(user))
+                .catch(err => console.log(err))
             })
-            console.log(newUser)
-            newUser.save()
+         })
         }
-        //    think about incorporating validator npm package... 
-        //    need validation to check things
     })
 })
 
