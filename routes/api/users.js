9e174ebdf1022e64f7a450eeffad5dbf6ade6a48
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2019-01-07 16:02:52 -0800

    finishing users auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,28 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs'); 
-
+const jwt = require('jsonwebtoken')
+const jsonkey = require('../../config/jsonkey')
 // Validation Flies
 const validateSignup = require('../../validation/signup')
+const validateSignin = require('../../validation/signin')
 //Load User models
 const Users = require('../../models/Users');
 
 router.get('/test', ( req, res ) => res.json({msg:"Users Works"
 }))
 
-// @register new users 
+// @signup new users 
+// Creating a new user
+
 router.post('/signup', ( req, res ) => {
    
     const { errors, isValid } = validateSignup(req.body);
 
     // Check Validation
     if (!isValid) {
-        console.log(req.body)
         return res.status(400).json(errors)
     }
     Users.findOne({email: req.body.email}).then(user => {
         if(user){
-            return res.status(400).json('User Already Exist');
+            error.email = 'Email Already Exist'
+            return res.status(400).json(errors);
         } else{
             const newUser = new Users({
                 email: req.body.email,
@@ -44,10 +48,41 @@ router.post('/signup', ( req, res ) => {
 // @public router
 // @sign-in for exisiting users 
 router.get('/signin', ( req, res ) => {
-    res.json({
-        email: "pablo",
-        password: "jose@email"
+    const { errors, isValid } = validateSignin(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        return res.status(400).json(errors)
+    }
+    const email = req.body.email 
+    const password = req.body.password
+
+    Users.findOne({email}).then(user => {
+        //check for user
+        if(!user) {
+            errors.email = "User not found"
+            return res.status(404).json(errors)
+        }
+        //password check
+        bcrypt.compare(password , user.password).then(isMatch => {
+            if(isMatch){
+                const payload = { id: user.id , } // jwt payload
+                jwt.sign(payload ,
+                     jsonkey.secretOrKey,
+                      { expiresIn: 3600 }, 
+                      (err, token) => {
+                        res.json({
+                            success: true,
+                            token: 'Bearer ' + token
+                        })
+                })
+            }else {
+                return res.status(400).json({pasword: 'Password Incorrect'})
+            }
+        })
     })
+
+
 })
 
 module.exports = router;
