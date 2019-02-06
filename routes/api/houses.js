8d95e77df9a675bc1b4c006a0a26755861455130
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2019-02-05 23:54:39 -0800

    starting rought draft on front end

--- routes/api/houses.js
+++ routes/api/houses.js
@@ -11,12 +11,29 @@ const Houses = require('../../models/Houses');
 router.get('/test', ( req, res ) => res.json({msg:"Users Works"
 }))
 
-router.get('/home' ,( req,res ) => {
+router.get('/current/home' ,( req,res ) => {
     // display all the home that are avalible
-})
+    const errors = {};
 
+    Profiles.find()
+    .populate('user' , 'images', 'street' ,'state' , 'city', 'price', 'created'  ) // going to add house here to populate
+        .then(profiles => {
+        if (!profiles) {
+          errors.noprofile = 'There are no profiles';
+          return res.status(404).json(errors);
+        }
+        
+        res.json(profiles);
+      })
+      .catch(err => res.status(404).json({ profile: 'There are no profiles' }));
+  });
 
+//   router.get('/current/home/:id', ( req , res ) => {
+//     Profiles.findById(req.params.id)
+//     .then(posts => res.json(posts))
+//     .catch(err => 
+//         res.status(404).json({ nopostfound: 'No post found with that ID'})
+//         );
+//     })
 
-
-
-module.exports = router;
+  module.exports = router;
